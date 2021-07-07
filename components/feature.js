d34b9a74f79349b70723290466410d4cbc5c9439
Raj Dutta <rd2249619@gmail.com>
2021-07-07 11:13:45 +0600

    modifying the feature container

--- components/feature.js
+++ components/feature.js
@@ -1,17 +1,48 @@
 import styles from "./../stylesheets/feature.module.scss";
+import { useState, useEffect } from "react";
 import { CgWebsite } from "react-icons/cg";
 import { MdDeveloperMode } from "react-icons/md";
 import { FaMobileAlt } from "react-icons/fa";
+import { animated, useSpring } from "react-spring";
+import { useInView } from 'react-intersection-observer';
+import * as d3 from "d3-ease";
 
 function Feature({ colors }) {
 
     const mainBackgroundColor = ( colors.textColor === "black" ? "#ff4700" : colors.backgroundColor === "#fe4c78" ? "#8ae4ff" : colors.backgroundColor === "#7833C2" ? "#69f0ae" : colors.backgroundColor === "#561d5e" ? "#6c63ff" : colors.backgroundColor === "#7852ff" ? "#00c4ff" : colors.textColor );
 
+    const [ state, setState ] = useState(false);
+
+    const { ref: featureRef, inView: featureView } = useInView({
+        threshold: 0,
+        triggerOnce: true
+      });
+
+      useEffect(() => {
+        setState(featureView);
+      }, [featureView]);
+
+    const { top, opacity } = useSpring({
+        from: {
+            top: -20,
+            opacity: 0
+        },
+        to: {
+            top: ( state ) ? 0 : -20,
+            opacity: ( state ) ? 1 : 0
+        },
+        delay: 300,
+        config: {
+            duration: 1200,
+            easing: d3.easeLinear
+        }
+    });
+
     return (
         <>
             <div className={styles.feature_work}>
                 <h3 className={styles.featured_work_main_title} style={{ backgroundColor: mainBackgroundColor }}>Feature Work</h3>
-                <div className={styles.feature_container}>
+                <animated.div style={{ transform: top.to(v => `translateY(${v}px)`), opacity: opacity.to(v => v) }} ref={featureRef} className={styles.feature_container}>
                     <div className={styles.items}>
                         <span style={{ backgroundColor: mainBackgroundColor }}><CgWebsite color="#fff" size={20} /></span>
                         <h4>Web Design</h4>
@@ -27,7 +58,7 @@ function Feature({ colors }) {
                         <h4>Web Application</h4>
                         <p>Building web applications with complexity, sustainability and provide facilities and securities features. Providing SQL & NoSQL facilities with a secure way and user-friendly interface.</p>
                     </div>
-                </div>
+                </animated.div>
             </div>
         </>
     );
