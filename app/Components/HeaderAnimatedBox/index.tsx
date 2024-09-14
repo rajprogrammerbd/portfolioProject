49d71be39c0c4d20902201a4518403b81c3f47b7
Raj Dutta <rd2249619@gmail.com>
2024-09-14 12:16:22 +0600

    fixed the design animation of header container

--- app/Components/HeaderAnimatedBox/index.tsx
+++ app/Components/HeaderAnimatedBox/index.tsx
@@ -1,12 +1,9 @@
 'use client';
 import { useState, useEffect } from "react";
 import styled from "styled-components";
-import { useDispatch, useSelector } from "react-redux";
 import { ThreeDots } from 'react-loader-spinner'
-import { changeWebLoadedOnce } from "@/lib/reducers/HomeReducer";
-import { motion, AnimatePresence } from "framer-motion";
+import { motion } from "framer-motion";
 import { HeaderAnimatedBoxState, IHeaderProps } from "@/types/Home";
-import { RootState } from "@/lib/store";
 
 const AnimationStyledBox = styled.div`
   background-color: white;
@@ -15,7 +12,7 @@ const AnimationStyledBox = styled.div`
   align-items: center;
   justify-content: center;
   position: fixed;
-  top: 0;
+  top: 0%;
   width: 100dvw;
   height: 100dvh;
   max-height: 100dvh;
@@ -26,11 +23,8 @@ const AnimationStyledBox = styled.div`
 const AnimatedBox = motion(AnimationStyledBox);
 
 const HeaderAnimatedBox = (props: IHeaderProps) => {
-  const dispatch = useDispatch();
+  const [load, setLoad] = useState<boolean>(false);
   const [state, setState] = useState<HeaderAnimatedBoxState>(() => {
-    // const width = global.window && global.window.innerWidth;
-    // const height = global.window && global.window.innerHeight;
-
     return {
       width: 0,
       height: 0
@@ -38,7 +32,16 @@ const HeaderAnimatedBox = (props: IHeaderProps) => {
   });
 
   const { color1 } = props;
-  const isLoaded = useSelector((root: RootState) => root.homepage.loaded);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoad(true);
+    }, 500);
+
+    return () => {
+      clearTimeout(timeout);
+    }
+  }, []);
 
   useEffect(() => {
     const fn = () => {
@@ -53,26 +56,9 @@ const HeaderAnimatedBox = (props: IHeaderProps) => {
     }
   }, []);
 
-  useEffect(() => {
-    // This hook handled the loading animation play onces.
-    const timeFn = () => {
-      if (!isLoaded) {
-        dispatch(changeWebLoadedOnce());
-      }
-    }
-
-    const timed = setTimeout(timeFn, 1500);
-    
-
-    return () => {
-      window.clearTimeout(timed);
-    }
-  }, []);
-
   return (
-    <AnimatePresence>
     <AnimatedBox
-      animate={{ y: isLoaded ? -(state.height) : 0 }}
+      animate={{ top: !load ? '0%' : '-100%' }}
       transition={{ duration: 1.6, ease: 'circIn' }}
     >
       <ThreeDots
@@ -85,7 +71,6 @@ const HeaderAnimatedBox = (props: IHeaderProps) => {
       />
       <p>Loading...</p>
     </AnimatedBox>
-    </AnimatePresence>
   );
 }
 
